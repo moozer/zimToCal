2fd95882d1283df7ef6c3981c1bf62d298b36243
moz <moz@local>
2017-04-08 00:18:28 +0200

    moved query tasks to its own function added priority to fetshed data

--- taskListReader.py
+++ taskListReader.py
@@ -20,7 +20,9 @@ def extractTime( taskText ):
     newText = parser.sub( '', taskText)
     return (timeText, newText)
 
-task_record = namedtuple('task_record', ["date", "description", "time", "open", "tags", "path"])
+task_record = namedtuple('task_record',
+                         ["date", "description", "time",
+                          "open", "tags", "path", "priority"])
 
 
 class taskListReader( object ):
@@ -47,30 +49,17 @@ class taskListReader( object ):
     '''
     def __init__( self, config ):
         self.dbfilename = config.filename
-
+        self.config = config
         self.con = sqlite3.connect( self.dbfilename )
-        self.cur = self.con.cursor()
 
-        # 9999 is the magic number for "no due date"
-        query = 'select due, description, open, tags, source from tasklist where due != "9999"'
-
-        if config.closed_tasks:
-            query += " and open = 0"
-        else:
-            query += " and open = 1"
-
-        if not config.limit_tags:
-            self.cur.execute( query )
-        else:
-            self.cur.execute( query + ' and tags=?', (config.limit_tags, ) )
+        self.tasks_query_cur = self._query_tasks()
 
     def __iter__( self ):
         return self
 
     def next( self ):
         try:
-            due, description, open_status, tags, source_id = self.cur.fetchone()
-
+            due, description, open_status, tags, source_id, prio = self.tasks_query_cur.fetchone()
             path=self._get_parent_pages( source_id )
 
             result = re.sub('\[.*\]', '', description)
@@ -79,15 +68,33 @@ class taskListReader( object ):
             nexttask = task_record(
                         date=date( y,m,d ), description=newText,
                         time=timeText, open=open_status,
-                        tags=tags, path=path )
+                        tags=tags, path=path, priority=prio )
 
             return nexttask
 
         except ValueError:
             raise ValueError( "Possible date error in task '%s'"%description)
 
-        except TypeError:
+        except TypeError, ex:
             raise StopIteration
+
+    def _query_tasks( self ):
+        cur = self.con.cursor()
+
+        # 9999 is the magic number for "no due date"
+        query = 'select due, description, open, tags, source, prio from tasklist where due != "9999"'
+
+        if self.config.closed_tasks:
+            query += " and open = 0"
+        else:
+            query += " and open = 1"
+
+        if not self.config.limit_tags:
+            cur.execute( query )
+        else:
+            cur.execute( query + ' and tags=?', (self.config.limit_tags, ) )
+
+        return cur
 
     def _get_parent_pages( self, pageid ):
         parent_id, basename = self._get_parent_page( pageid )
