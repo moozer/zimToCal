7e7a887c3beb28d2b43523719218c2bc6c7e7dd7
moz <moz@local>
2017-04-07 11:57:33 +0200

    added zimToStdout

---
+++ zimToStdout.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+# outputs the tasks from the iterator as ical to stdout
+#
+# adapted from here:
+#  http://icalendar.readthedocs.org/en/latest/usage.html
+
+from taskListReader import taskListReader, extractTime
+import argparse
+
+def setupArgparse():
+    parser = argparse.ArgumentParser(description='zimToCal convert zim tasks to ics.')
+    parser.add_argument("filename", help="the index.db file to use" )
+    parser.add_argument('-t','--limit-tags',   help='Include only tasks with this tag', required=False)
+    parser.add_argument('-c','--closed-tasks', help='Show only closed tasks (default: show only open tasks)',
+                                               required=False, action='store_true')
+
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    config = setupArgparse()
+
+    for t in taskListReader( config ):
+        timeText, newText = extractTime( t['description'] )
+        print "%s\t%s\t%s"%(t['date'], timeText if timeText else "", newText.strip())
