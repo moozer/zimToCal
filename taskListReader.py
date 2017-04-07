fd3dee1d8f3bacb4fab6b228eadad43060cc4359
moz <moz@local>
2017-04-07 11:45:33 +0200

    fixed config object when run as test

--- taskListReader.py
+++ taskListReader.py
@@ -5,10 +5,10 @@ import re
 from datetime import date
 
 class taskListReader(  ):
-    ''' reads the tasklist cache file and outputs 
-    
+    ''' reads the tasklist cache file and outputs
+
     and becomes an iterable object
-    
+
     From the database
 
     sqlite> .schema tasklist
@@ -28,26 +28,26 @@ class taskListReader(  ):
     '''
     def __init__( self, config ):
         self.dbfilename = config.filename
-    
+
         self.con = sqlite3.connect( self.dbfilename )
         self.cur = self.con.cursor()
- 
+
         # 9999 is the magic number for "no due date"
         query = 'select due, description from tasklist where due != "9999"'
-        
+
         if config.closed_tasks:
             query += " and open = 0"
         else:
             query += " and open = 1"
-            
+
         if not config.limit_tags:
             self.cur.execute( query )
         else:
             self.cur.execute( query + ' and tags=?', (config.limit_tags, ) )
- 
+
     def __iter__( self ):
         return self
-        
+
     def next( self ):
         row = self.cur.fetchone()
         if not row:
@@ -57,16 +57,25 @@ class taskListReader(  ):
 
         try:
             y,m,d = [int(i) for i in row[0].split('-')]
-            nexttask = { "date": date( y,m,d ), 
+            nexttask = { "date": date( y,m,d ),
                         "description": result }
         except ValueError:
             raise ValueError( "Possible date error in task '%s'"%row[1] )
-            
-        return nexttask        
 
-    
+        return nexttask
+
+
 # a simple test to show syntax
+class AttrDict(dict):
+    def __init__(self, *args, **kwargs):
+        super(AttrDict, self).__init__(*args, **kwargs)
+        self.__dict__ = self
+
 if __name__ == "__main__":
-    tasks = taskListReader( "testData/index.db" )
+    config = { "filename": "testData/index.db",
+                "closed_tasks": False,
+                "limit_tags": None}
+
+    tasks = taskListReader( AttrDict( config ) )
     for t in tasks:
         print t
