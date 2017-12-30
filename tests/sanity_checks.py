655aa0f8607c9cfbf24f25fb74dde00fa303aa3a
moz <moz@local>
2017-12-30 11:19:57 +0100

    now exposeing task_record also

--- tests/sanity_checks.py
+++ tests/sanity_checks.py
@@ -1,13 +1,32 @@
 import unittest
-
 from zimToCal import *
+import datetime
 
-test_config = ConfigStruct( filename='../testData/index.db',
-                            not_open_tasks=False,
-                            closed_tasks=False,
-                            limit_tags=None)
+test_config = ConfigStruct(filename='../testData/index.db',
+                           not_open_tasks=False,
+                           closed_tasks=False,
+                           limit_tags=None)
+
+test_task = task_record(date="2017-12-11",
+                        description="some description",
+                        time="10:00",
+                        open=True,
+                        tags=['aTag'],
+                        path=["first", "second"],
+                        priority=4,
+                        reach=30,
+                        parent_id=10,
+                        id=15,
+                        datetime=datetime.datetime.strptime("2017-12-11 10:00", "%Y-%m-%d %H:%M"))
+
 
 class MyTestCase(unittest.TestCase):
+
+    def test_config_struct(self):
+        self.assertEqual(test_config.filename, '../testData/index.db')
+
+    def test_task_record(self):
+        self.assertEqual(test_task.time, "10:00")
 
     def test_setup_arg_parse(self):
         with self.assertRaises(SystemExit):
@@ -15,10 +34,10 @@ class MyTestCase(unittest.TestCase):
             setup_arg_parse()
 
     def test_task_to_stdout(self):
-        task_to_stdout( test_config )
+        task_to_stdout(test_config)
 
     def test_task_to_cal(self):
-        print task_to_cal( test_config )
+        print task_to_cal(test_config)
 
 
 if __name__ == '__main__':
--- zimToCal/__init__.py
+++ zimToCal/__init__.py
@@ -1,2 +1,3 @@
 from zimToCal import task_to_cal, setup_arg_parse, ConfigStruct
 from zimToStdout import task_to_stdout
+from taskListReader import task_record
